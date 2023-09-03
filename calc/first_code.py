d084ddca598ce97ec0eb21c98a41e26430e87b41
Vadim Zorin <vadimmmz@mail.ru>
2023-09-03 20:04:23 +0300

    Start to create calculator feature

--- calc/first_code.py
+++ calc/first_code.py
@@ -3,17 +3,25 @@ import argparse
 def calc_addition(a, b):
     return a+b
 
+def calc_multiplication(a, b):
+    return a*b
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--a", '-a', help="The first number")
-    parser.add_argument("--b", '-b', help="The second number")
+    parser.add_argument("--first", '-f', help="The first number")
+    parser.add_argument("--second", '-s', help="The second number")
+    parser.add_argument("--action", '-a', help="The second number")
     args = parser.parse_args()
 
     try:
-        if args.a and args.b:
-            print(calc_addition(int(args.a), int(args.b)))
+        if args.first and args.second:
+            if args.action == 'add':
+                print(calc_addition(int(args.first), int(args.second)))
+            elif args.action == 'mult':
+                print(calc_multiplication(int(args.first), int(args.second)))
+            else:
+                print("-a parameter not recieved, you should declare -a parameter ")
     except(TypeError):
         print('a and b must be integer')
     except(ValueError):
